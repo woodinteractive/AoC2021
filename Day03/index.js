797d821a08fc1b46494ea8711857c2c7f4ebe74c
Daniel <dwood@ksturnpike.com>
2021-12-06 01:36:03 -0600

    Restore correct day 3 solution

--- Day03/index.js
+++ Day03/index.js
@@ -1,124 +1,43 @@
 import fs from 'fs';
-const input = fs.readFileSync('input.txt').toString().trim().split('\n\n')
-const numbers = input.shift().split(',').map(n => parseInt(n))
-const boards = input.map((board) => board.split('\n')).map((board) => {
-  return board.map((row) => row.trim().split(/\s+/).map(n => parseInt(n)))
-})
+const input = fs.readFileSync('input.txt').toString().trim().split('\n')
 
-const checkBoard = (index, marks) => {
-  const x = []
-  const y = []
-  let countX = {}
-  let countY = {}
-  let bingo = false
+// Part 1 Solution
+const binaryArray = input.map((binary) => binary.split('').map((n) => parseInt(n)))
+const start = Array(binaryArray[0].length).fill(0, 0, binaryArray[0].length)
+const result = binaryArray.reduce((previousValue, currentValue, index) => {
+  currentValue.forEach((value, index) => {
+    value === 1 ? previousValue[index]++ : previousValue[index]--
+  })
+  return previousValue
+}, start)
 
-  if(marks[index]) {
-    for (let i = 0; i < marks[index].length; i++) {
-      x.push(marks[index][i][0])
-      y.push(marks[index][i][1])
-    }
-  }
-  for (const num of x) { 
-    countX[num] = countX[num] ? countX[num] + 1 : 1 
-    for (let i = 0; i < 5 && !bingo; i++) {
-      if(countX[i] >= 5) bingo = { x: countX[i] }
-    }
-  }
-  for (const num of y) { 
-    countY[num] = countY[num] ? countY[num] + 1 : 1 
-    for (let i = 0; i < 5 && !bingo; i++) {
-      if(countY[i] >= 5) bingo = { y: countY[i] }
-    }
-  }
-  return bingo
-}
+const gamma = parseInt(result.map((n) => n > 0 ? 1 : 0).join(''), 2)
+const epsilon = parseInt(result.map((n) => n > 0 ? 0 : 1).join(''), 2)
 
-const findFirstWinner = (numbers, boards) => {
-  const called = []
-  const marks = {}
-  let bingo = false
-  let winner = {}
-
-  for (let i = 0; i < numbers.length && !bingo; i++) {
-    called.push(numbers[i])
-    for (const [boardIndex, board] of boards.entries()) {
-      for (const [rIdx, row] of board.entries()) {
-        for (const [column, n] of row.entries()) {
-          if(numbers[i] === n) {
-            marks[boardIndex] ? marks[boardIndex].push([rIdx, column]) : marks[boardIndex] = [[rIdx, column]]
-            break
-          }
-        }
-      }
-      const result = checkBoard(boardIndex, marks)
-      if(result) {
-        bingo = true
-        winner.line = result
-        winner.index = boardIndex
-        winner.board = board
-        winner.marks = marks[boardIndex]
-        winner.lastCalled = called.pop()
-        break
-      }
-    }
-  }
-  return winner
-}
-
-const getScore = (winner) => {
-  return winner.board.map((row, index) => {
-    const marks = winner.marks.map(mark => {
-      if(mark[0] === index) {
-        return mark[1]
-      }
-    }).filter(mark => mark !== undefined)
-    const filtered = row.filter((n, i) => !marks.includes(i))
-    if(filtered.length) {
-      return filtered.reduce((acc, curr) => acc+curr)
-    } 
-  }).filter((total) => total !== undefined).reduce((acc, curr) => acc+curr)*winner.lastCalled
-}
-
-const firstWinner = findFirstWinner(numbers, boards)
-const score = getScore(firstWinner)
-console.log(`part 1: ${score}`)
+console.log(`Part 1 solution: ${gamma*epsilon}`)
 
 // Part 2 Solution
-const findLastWinner = (numbers, boards) => {
-  const totalBoards = boards.length
-  const called = []
-  const marks = {}
-  let bingoCount = 0
-  let bingoBoards = []
-  let winner = {}
+let oxygen = input
+let CO2 = input
 
-  for (let i = 0; i < numbers.length && bingoBoards.length !== totalBoards; i++) {
-    called.push(numbers[i])
-    for (const [boardIndex, board] of boards.entries()) {
-      for (const [rIdx, row] of board.entries()) {
-        for (const [column, n] of row.entries()) {
-          if(numbers[i] === n) {
-            marks[boardIndex] ? marks[boardIndex].push([rIdx, column]) : marks[boardIndex] = [[rIdx, column]]
-            break
-          }
-        }
-      }
-      if(checkBoard(boardIndex, marks) && !bingoBoards.includes(boardIndex)) {
-        bingoBoards.push(boardIndex)
-      }
-      if(bingoBoards.length === totalBoards) {
-        winner.line = checkBoard(boardIndex, marks)
-        winner.index = boardIndex
-        winner.board = board
-        winner.marks = marks[boardIndex]
-        winner.lastCalled = called.pop()
-        break
-      }
-    }
+for (let i = 0; i < input[0].length && oxygen.length > 1; i++) {
+  const zero = []
+  const one = []
+
+  for (const binary of oxygen) {
+    parseInt(binary[i]) > 0 ? one.push(binary) : zero.push(binary)
   }
-  return winner
+  oxygen = one.length >= zero.length ? one : zero
 }
 
-const lastWinner = findLastWinner(numbers, boards)
-const score2 = getScore(lastWinner)
-console.log(`part 2: ${score2}`)
+for (let i = 0; i < input[0].length && CO2.length > 1; i++) {
+  const zero = []
+  const one = []
+
+  for (const binary of CO2) {
+    parseInt(binary[i]) > 0 ? one.push(binary) : zero.push(binary)
+  }
+  CO2 = one.length >= zero.length ? zero : one
+}
+
+console.log(`Part 2 Solution: ${parseInt(oxygen.join(''), 2)*parseInt(CO2.join(''), 2)}`)
