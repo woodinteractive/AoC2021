25de9cad3f6e044965548884ae9c94b5bbe822da
Daniel Wood <wood4283@gmail.com>
2021-12-11 10:34:26 -0600

    Day 9 solution

--- Day09/index.js
+++ Day09/index.js
@@ -1,90 +1,78 @@
 import fs from 'fs'
-const input = fs.readFileSync('input.txt').toString().trim().split('\n').map(row => row.split(''))
+const input = fs.readFileSync('input.txt').toString().trim().split('\n').map(row => row.split('').map(n => parseInt(n)))
 const test = fs.readFileSync('example.txt').toString().trim().split('\n').map(row => row.split('').map(n => parseInt(n)))
 
-const getLowPoints = (input) => {
-  const lowPoints = []
-  
-  const isLessThanUp = (grid, y, x) => y === 0 || grid[y-1][x] > grid[y][x]  ? true : false
-  const isLessThanDown = (grid, y, x) => y === grid.length-1 || grid[y+1][x] > grid[y][x] ? true : false
-  const isLessThanLeft = (row, x) => x === 0 || row[x-1] > row[x] ? true : false
-  const isLessThanRight = (row, x) => x === row.length-1 || row[x+1] > row[x] ? true : false
-  
-  for (const [y, row] of input.entries()) {
-    for (const [x, num] of row.entries()) {
-      if(isLessThanUp(input, y, x) && isLessThanDown(input, y, x) && isLessThanLeft(row, x) && isLessThanRight(row, x)) {
-        lowPoints.push({x, y, num})
+const isLower = (y, x, grid) => {
+  const h = grid.length
+  const w = grid[0].length
+  const tile = grid[y][x]
+
+  if (y > 0 && grid[y - 1][x] <= tile) return false // top
+  if (x > 0 && grid[y][x - 1] <= tile) return false // left
+  if (x < w - 1 && grid[y][x + 1] <= tile) return false // right
+  if (y < h - 1 && grid[y + 1][x] <= tile) return false // bottom
+
+  return true
+}
+
+const getLowPoints = (grid) => {
+  let answer = 0
+  const height = grid.length
+  const width = grid[0].length
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (isLower(y, x, grid)) {
+        answer += grid[y][x] + 1
       }
     }
   }
-  return lowPoints
-}
 
-const addPoints = (points) => points.map(n => parseInt(n.num)+1).reduce((a,b) => a+b)
+  return answer
+}
 
 // Part 1 Solution
-const points = getLowPoints(test)
-console.log(`part 1: ${addPoints(points)}`)
+console.log(`part 1: ${getLowPoints(input)}`)
 
 // Part 2 Solution
-const getLeft = (start, grid) => {
-  const found = []
-  let i = start.x
-  while(grid[start.y][i] !== 9 && i > 0) {
-    found.push({ y: start.y, x: i })
-    i--
+
+
+const getBasin = (y, x, grid, checked = []) => {
+  const h = grid.length
+  const w = grid[0].length
+
+  if (x < 0 || x >= w || y < 0 || y >= h) return 0
+  if (grid[y][x] == 9) return 0
+
+  const key = `${y}_${x}`
+  if (checked.includes(key)) return 0
+
+  let count = 1 // itself
+  checked.push(key)
+
+  count += getBasin(y - 1, x, grid, checked)
+  count += getBasin(y + 1, x, grid, checked)
+  count += getBasin(y, x - 1, grid, checked)
+  count += getBasin(y, x + 1, grid, checked)
+
+  return count
+}
+
+const getBasins = (grid) => {
+  const h = grid.length
+  const w = grid[0].length
+  const basins = []
+  
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      if (isLower(y, x, grid)) {
+        basins.push(getBasin(y, x, grid))
+      }
+    }
   }
-  return found
+  
+    const sorted = basins.sort((a, b) => b - a)
+    return sorted[0] * sorted[1] * sorted[2]
 }
 
-const getRight = (start, grid) => {
-  const found = []
-  let i = start.x
-  while(grid[start.y][i] !== 9 && i < grid[start.y].length-1) {
-    found.push({ y: start.y, x: i })
-    i++
-  }
-  return found
-}
-
-const getAbove = (start, grid) => {
-  const found = []
-  let i = start.y
-  while(grid[i][start.x] !== 9 && i < 0) {
-    console.log(i)
-    found.push({ y: i, x: start.x })
-    i--
-  }
-  return found
-}
-
-const getBelow = (start, grid) => {
-  const found = []
-  let i = start.y
-  while(grid[i][start.x] !== 9 && i < grid.length-1) {
-    found.push({ y: i, x: start.x })
-    i++
-  }
-  return found
-}
-const removeDups = (arr) => arr.filter((v, i, a) => a.map(c => JSON.stringify(c)).indexOf(JSON.stringify(v)) === i)
-
-const row = [...getLeft(points[2], test), ...getRight(points[2], test)]
-
-let second = []
-for (const start of removeDups(row)) {
-  second = [...second, ...getAbove(start, test)]
-  second = [...second, ...getBelow(start, test)]
-}
-// console.log(removeDups(second).sort((a,b) => a.y - b.y))
-
-
-// const getBasins = (points, grid) => {
-//   const basins = []
-//   for (const point of points) {
-//     console.log(point)
-//   }
-// }
-
-// getBasins(points, test)
-// console.log(`part 2: `)
+console.log(`part 2: ${getBasins(input)}`)
